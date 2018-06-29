e60ee5d90b86f8fd8b5ad539f549c3c5f4a7d345
Hung Phan <hungpv@atmarkcafe.org>
2018-06-29 10:48:14 +0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProfessionalRails5DevelopmentCourse::Application.config.secret_token = 'da7b4ea03d7faf9adcc237c145e32b6b674c5a9ccef954226b49a867e90b3f0fba0b46e37e3ed526c7b67c0832a7c2faa5d2466caec356afbe823e9e36277e4d'
